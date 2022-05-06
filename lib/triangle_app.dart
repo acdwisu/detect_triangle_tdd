2e4e7ce335ff0902d69ed30b48043693d7bb4cfb
acdwisu <acdwisu@outlook.com>
2022-05-06 14:36:51 +0700

    triangle unequality check

--- lib/triangle_app.dart
+++ lib/triangle_app.dart
@@ -4,6 +4,12 @@ TriangleType? detectTriangle(num side1, num side2, num side3) {
   final sides = [side1, side2, side3];
 
   int countEqual = 0;
+
+  sides.sort();
+
+  if(sides[0] + sides[1] <= sides[2]) {
+    throw Exception('Inequal Triangle');
+  }
   
   sides.fold<List<num>>([0], (previousValue, element) {
     if(element < 1) {
--- test/triangle_app_test.dart
+++ test/triangle_app_test.dart
@@ -14,28 +14,37 @@ void main() {
       expect(() => detectTriangle(-1,-2,-2), throwsA(isA<Exception>()));
     });
 
+    test('Should throw Error When 2 shorter side <= longest side (triangle inquality)', () {
+      expect(() => detectTriangle(4,1,2), throwsA(isA<Exception>()));
+      expect(() => detectTriangle(5,1,3), throwsA(isA<Exception>()));
+    });
+
     test('Should return SamaSisi Type when all sides are equal', () {
       expect(detectTriangle(1, 1, 1), TriangleType.SamaSisi);
       expect(detectTriangle(2, 2, 2), TriangleType.SamaSisi);
-      expect(detectTriangle(1, 2, 2), isNot(TriangleType.SamaSisi));
+      expect(detectTriangle(200, 200, 200), TriangleType.SamaSisi);
+      expect(detectTriangle(5, 3, 4), isNot(TriangleType.SamaSisi));
       expect(detectTriangle(2, 2, 1), isNot(TriangleType.SamaSisi));
-      expect(detectTriangle(2, 1, 2), isNot(TriangleType.SamaSisi));
     });
 
     test('Should return SamaKaki Type when 2 sides are equal', () {
-      expect(detectTriangle(2, 1, 1), TriangleType.SamaKaki);
+      expect(detectTriangle(2, 4, 4), TriangleType.SamaKaki);
       expect(detectTriangle(2, 1, 2), TriangleType.SamaKaki);
-      expect(detectTriangle(1, 1, 2), TriangleType.SamaKaki);
+      expect(detectTriangle(3, 3, 1), TriangleType.SamaKaki);
+      expect(detectTriangle(2, 2, 1), TriangleType.SamaKaki);
+      expect(detectTriangle(100, 3, 100), TriangleType.SamaKaki);
+      expect(detectTriangle(200, 200, 2), TriangleType.SamaKaki);
+      expect(detectTriangle(4, 1, 4), TriangleType.SamaKaki);
       expect(detectTriangle(1, 1, 1), isNot(TriangleType.SamaKaki));
-      expect(detectTriangle(2, 1, 3), isNot(TriangleType.SamaKaki));
+      expect(detectTriangle(5, 7, 3), isNot(TriangleType.SamaKaki));
     });
 
     test('Should return TakBeraturean Type when no sides are equal', () {
-      expect(detectTriangle(2, 5, 1), TriangleType.TakBeraturan);
-      expect(detectTriangle(1, 4, 9), TriangleType.TakBeraturan);
+      expect(detectTriangle(2, 5, 6), TriangleType.TakBeraturan);
+      expect(detectTriangle(6, 4, 9), TriangleType.TakBeraturan);
       expect(detectTriangle(7, 6, 2), TriangleType.TakBeraturan);
       expect(detectTriangle(1, 1, 1), isNot(TriangleType.TakBeraturan));
-      expect(detectTriangle(2, 1, 2), isNot(TriangleType.TakBeraturan));
+      expect(detectTriangle(2, 4, 4), isNot(TriangleType.TakBeraturan));
     });
   });
 }
